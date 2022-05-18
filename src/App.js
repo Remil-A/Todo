743ece1f916930fd5cf1848519cb8bd5d4093c75
Adu Sarah <adusarah@Adus-Mackbook.local>
2022-05-18 12:30:27 +0100

    Error fix

--- src/App.js
+++ src/App.js
@@ -1,103 +1,105 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from "react";
+import "./App.css";
 
-
-function App() {
-  const [todos, setTodos] = useState([])
-  const [todo, setTodo] = useState("")
-  const [todoEditing, setTodoEditing] = useState(null)
-  const [editingText, setEditingText] = useState("")
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
 
   useEffect(() => {
-    const temp = localStorage.getItem("todos")
-    const loadedTodos = JSON.parse(temp)
-
-    if(loadedTodos){
-      setTodos(loadedTodos)
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    const temp = JSON.stringify(todos)
-    localStorage.setItem("todos", temp)
-  }, [todos])
-  
+    const json = JSON.stringify(todos);
+    localStorage.setItem("todos", json);
+  }, [todos]);
 
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
 
     const newTodo = {
       id: new Date().getTime(),
       text: todo,
-      completed: false
+      completed: false,
+    };
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
   }
-  setTodos([...todos].concat(newTodo))
-  setTodo("")
-  }
-  function deleteTodo(id){
-    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
 
-    setTodos(updatedTodos)
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
   }
-  function toggleComplete(id){
-    const updatedTodos = [...todos].map((todo) => {
-      if(todo.id === id){
-        todo.completed = !todo.completed
+
+  function toggleComplete(id) {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
       }
-      return todo
-    })
-    setTodos(updatedTodos)
+      return todo;
+    });
+    setTodos(updatedTodos);
   }
-  function editTodo(id){
+
+  function submitEdits(id) {
     const updatedTodos = [...todos].map((todo) => {
-      if(todo.id === id){
-        todo.text = editingText
+      if (todo.id === id) {
+        todo.text = editingText;
       }
-      return todo
-    })
-    setTodo(updatedTodos)
-    setTodoEditing(null)
-    setEditingText("")
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
   }
+
   return (
-    <section className="todo-list">
+    <div id="todo-list">
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-      <div className="form-input">
-      <input 
-        type="text"
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-      />
-      <button type="submit">Add Todo</button>
-      
-      </div>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
       </form>
       {todos.map((todo) => (
-        <div key={todo.id}>
-          {todoEditing === todo.id ? (
+        <div key={todo.id} className="todo">
+          <div className="todo-text">
             <input
-              type="text"
-              onChange={(e) => setEditingText(e.target.value)}
-              value={editingText}
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
             />
-          ) : (
-            <div>{todo.text}</div>
-          )}
-          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          <input
-            type="checkbox"
-            onChange={() => toggleComplete(todo.id)}
-            checked={todo.completed}
-          />
-          {todoEditing === todo.id ? (
-            <button onClick={() => editTodo(todo.id)}>Submit Edit</button>
-          ) : (
-            <button onClick={() => setTodoEditing(todo.id)}>Edit todo</button>
-          )}
+            {todo.id === todoEditing ? (
+              <input
+                type="text"
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            ) : (
+              <div>{todo.text}</div>
+            )}
+          </div>
+          <div className="todo-actions">
+            {todo.id === todoEditing ? (
+              <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
+            ) : (
+              <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+            )}
+
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>
         </div>
       ))}
-    </section>
+    </div>
   );
-}
+};
 
-export default App
+export default App;
